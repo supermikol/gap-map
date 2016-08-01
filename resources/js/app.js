d69e647915656a21685f6b6400efbe0ad8f736c7
supermikol <mdu123@gmail.com>
2016-07-31 20:48:07 -0700

    add spinner for loading

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,4 @@
+
 $(document).ready(function(){
 
 })
-<div class="gm-style-iw" style="top: 9px; position: absolute; left: 15px; width: 332px;"><div style="display: inline-block; overflow: auto; max-height: 382px; max-width: 654px;"><div style="overflow: auto;">
-      <div id="iw-container">
-        <div class="iw-title">
-          94061
-          <span class="right">
-            Ratio: 411.32
-          </span>
-        </div>
-        <div class="iw-content">
-          <ol>
-            <li class="window-clinic"><a href="http://www.ymcasv.org/sequoia/html/programs_swimsportsplay.html#1" sequoia="" ymca="" -="" special="" needs="" swimming="" instruction<="" a="">1445 Hudson St, , Redwood City, CA, 94061</a></li><a href="http://www.ymcasv.org/sequoia/html/programs_swimsportsplay.html#1" sequoia="" ymca="" -="" special="" needs="" swimming="" instruction<="" a="">
-          </a></ol><a href="http://www.ymcasv.org/sequoia/html/programs_swimsportsplay.html#1" sequoia="" ymca="" -="" special="" needs="" swimming="" instruction<="" a="">
-        </a></div><a href="http://www.ymcasv.org/sequoia/html/programs_swimsportsplay.html#1" sequoia="" ymca="" -="" special="" needs="" swimming="" instruction<="" a="">
-        <div class="iw-bottom-gradient" style="display: none;"></div>
-      </a></div><a href="http://www.ymcasv.org/sequoia/html/programs_swimsportsplay.html#1" sequoia="" ymca="" -="" special="" needs="" swimming="" instruction<="" a="">
-    </a></div></div></div>
